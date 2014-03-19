349f043eab3e8e822f445e6e734956bde93b245e
mGleich <gm4288@web.de>
2014-03-19 01:46:08 +0100

    test

--- db/migrate/20130901145228_create_users.rb
+++ db/migrate/20130901145228_create_users.rb
@@ -7,7 +7,5 @@ class CreateUsers < ActiveRecord::Migration
 
       t.timestamps
     end
-
-    User.create  name:'Standart', password_digest:'$2a$10$OjhZKBcC9tBoi6FggX5t8u/MW8YBqc/1w87lQNR.q8RWKljPZRga2', role: 'Administrator'
   end
 end
