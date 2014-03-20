a269beaa11b439107d792909546ba36f5e043145
mGleich <gm4288@web.de>
2014-03-20 01:57:24 +0100

    test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,7 @@
 Lumia::Application.configure do
+
+  config.assets.precompile += %w( jquery.chained.js)
+
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests
